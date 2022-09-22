aaa4124e9de8de543b13c79c7480727f71dee4a2
Magnus <91549385+KoksILasten@users.noreply.github.com>
2022-09-22 19:09:35 +0200

    oijgegetg

--- src/components/CountryInfo.js
+++ src/components/CountryInfo.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
-import "../App.css";
+import React from 'react'
+import '../App.css'
 
-const CountryInfo = ({ data, details }) => {
+const CountryInfo = ({ data, details, max }) => {
   return (
-    <div className="CountryInfo">
-      <div className="top">
-        <p className="name">{data.name.common}</p>
-        <p className="area">
+    <div className='CountryInfo'>
+      <div className='top'>
+        <p className='name'>{data.name.common}</p>
+        <p className='area'>
           {(data.area / Math.pow(10, 6)).toFixed(1)} million km<sup>2</sup>
         </p>
       </div>
-      <div className="bar">
+      <div className='bar'>
         <div
-          className="bar-fill"
-          style={{ width: `${(data.area / 17098242) * 100}%` }}
+          className='bar-fill'
+          style={{ width: `${(data.area / max) * 100}%` }}
         />
       </div>
       {details && (
-        <div className="bottom">
-          <p className="capital">Capital: {data.capital}</p>
-          <p className="region">Region: {data.subregion}</p>
+        <div className='bottom'>
+          <p className='capital'>Capital: {data.capital}</p>
+          <p className='region'>Region: {data.subregion}</p>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default CountryInfo;
+export default CountryInfo
